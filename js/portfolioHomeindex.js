59ea57f0a32795fe5f6b18548acfd637202919ff
Jillbills <87965054+Jillbills@users.noreply.github.com>
2022-01-24 17:59:33 -0800

    Back to top button added

--- js/portfolioHomeindex.js
+++ js/portfolioHomeindex.js
@@ -44,52 +44,27 @@ function myFunction() {
 
 
 
+// BACK TO TOP BUTTON 
 
+//Get the button:
+mybutton = document.getElementById("bttBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-
-
-
-
-// Opens StickOutLoud website URL in a new tab when clicking a button 
-// window.open(url, "_blank");
-
-
-
-// Not currently active - need to reactivate HTML for this to work 
-// MODAL when you click Explore button in the About section of my homepage 
-// Get the modal 
-// var modal = document.getElementById("modalSOL");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("SOLbutton");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// //   modal.style.display = "#modalSOL"; 
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-
-
-
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
